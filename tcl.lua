3fc6d4e6d23d6c13860bf4af86293c063324ec74
753eml <112660352+753eml@users.noreply.github.com>
2024-10-05 18:27:37 +0300

    Update tcl.lua

--- tcl.lua
+++ tcl.lua
@@ -1,6 +1,7 @@
 loadstring(game:HttpGet('https://api.luarmor.net/files/v3/loaders/b95e8fecdf824e41f4a030044b055add.lua'))()
 local Upvote = "rbxassetid://97609256286565"
 local Downvote = "rbxassetid://99911273351388"
+
 local function sendNotification(title, text, duration, image)
     game:GetService("StarterGui"):SetCore("SendNotification", {  
         Title = title;
@@ -10,8 +11,8 @@ local function sendNotification(title, text, duration, image)
     })
 end
 
-if game.PlaceId = 286090429 then
-    sendNotification("You are in arsenal!", 10, Upvote)
+if game.PlaceId == 286090429 then
+    sendNotification("You are in Arsenal!", 10, Upvote)
 else
     sendNotification("You are not in Arsenal!", 10, Downvote)
 end
