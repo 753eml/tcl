2ec90fe56f3d3a048246df0551c36d1a779ce8f8
753eml <112660352+753eml@users.noreply.github.com>
2024-10-05 20:38:43 +0300

    Update tcl.lua

--- tcl.lua
+++ tcl.lua
@@ -2,15 +2,15 @@ local Upvote = "rbxassetid://97609256286565"
 local Downvote = "rbxassetid://99911273351388"
 local function sendNotification(title, text, duration, image)
     game:GetService("StarterGui"):SetCore("SendNotification", {  
-        Title = title;
-        Text = text;
-        Duration = duration; 
+        Title = title,
+        Text = text,
+        Duration = duration, 
         Icon = image
-}
+    })
 end
 if game.PlaceId == 286090429 then
-    sendNotification("Place Check", "You are in Arsenal!", 10, Upvote)
+    sendNotification("Correct game", "You are in Arsenal!", 10, Upvote)
     loadstring(game:HttpGet('https://api.luarmor.net/files/v3/loaders/b95e8fecdf824e41f4a030044b055add.lua'))()
 else
-    sendNotification("Place Check", "You are not in Arsenal!", 10, Downvote)
+    sendNotification("Incorrect game", "You are not in Arsenal!", 10, Downvote)
 end
